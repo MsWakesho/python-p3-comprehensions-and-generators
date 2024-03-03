077f3d62c2f18300fdadbd915a688afa7ba242eb
MsWakesho <wakeshosonja@gmail.com>
2024-03-03 20:00:56 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+  return [num for num in num_list if num % 2 == 0]
+num_list = [0, 1, 3, 5, 7, 8, 9]
+evens = (return_evens(num_list))
+print(evens)
+
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list ]
+
+sentence_list = ["I like computers","I require coffee","Live long and prosper"]
+sentences_with_exclamation = (make_exclamation(sentence_list))
+print(sentences_with_exclamation)
